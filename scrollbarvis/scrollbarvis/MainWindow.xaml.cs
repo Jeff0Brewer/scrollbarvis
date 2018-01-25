fd84acae182e416f3a8c71c41091496b9b88ef39
Jeff0Brewer <jeffreybrewer2019@u.northwestern.edu>
2018-01-25 15:09:21 -0600

    init

---
+++ scrollbarvis/scrollbarvis/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EyeXFramework.Wpf;
+using Tobii.EyeX.Framework;
+using EyeXFramework;
+
+namespace scrollbarvis
+{
+    public partial class MainWindow : Window
+    {
+        EyeXHost eyeXHost;
+        Point track = new Point(0, 0);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            eyeXHost = new EyeXHost();
+            eyeXHost.Start();
+            var gazeData = eyeXHost.CreateGazePointDataStream(GazePointDataMode.Unfiltered);
+            gazeData.Next += newGazePoint;
+
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer(System.Windows.Threading.DispatcherPriority.Render);
+            dispatcherTimer.Tick += new EventHandler(update);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            dispatcherTimer.Start();
+        }
+
+        private void newGazePoint(object s, EyeXFramework.GazePointEventArgs e)
+        {
+            track.X = e.X;
+            track.Y = e.Y;
+        }
+
+        private void update(object sender, EventArgs e)
+        {
+            Point currentGaze = PointFromScreen(track);
+        }
+    }
+}
